05016ec58eee35f06b06cbcc912bc53a807e656a
Will Lu <willorwgl@gmail.com>
2019-08-01 13:21:29 -0400

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 User.create(email: "flamingo@gmail.com", password: "Abc123456@")
